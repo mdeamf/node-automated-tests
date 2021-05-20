d4697284f968033cb5503da504624f23e816f012
mdeamf <mauriciodealmeidamf@gmail.com>
2021-05-20 15:12:46 -0300

    test: adding more number tests to app.js

--- app.test.js
+++ app.test.js
@@ -1,11 +1,31 @@
 const app = require('./app');
 
-test('Somar 1 + 2 = 3', () => {
+test('[NUMBER] 1 + 2 = 3', () => {
   const result = app.sumNumbers(1, 2);
   expect(result).toEqual(3);
 });
 
-test('Somar 0 + 0 = 0', () => {
+test('[NUMBER] 0 + 0 = 0', () => {
   const result = app.sumNumbers(0, 0);
   expect(result).toEqual(0);
+});
+
+test('[NUMBER] 5 + 6 > 9', () => {
+  const result = app.sumNumbers(5, 6);
+  expect(result).toBeGreaterThan(9);
+});
+
+test('[NUMBER] 1 + 3 >= 4', () => {
+  const result = app.sumNumbers(1, 3);
+  expect(result).toBeGreaterThanOrEqual(4);
+});
+
+test('[NUMBER] 10 + 1 < 15', () => {
+  const result = app.sumNumbers(10, 1);
+  expect(result).toBeLessThan(15);
+});
+
+test('[NUMBER] 50 + 40 <= 100', () => {
+  const result = app.sumNumbers(50, 40);
+  expect(result).toBeLessThanOrEqual(100);
 });
