3705f00107fe0b22ba03bf4632245db5a4af834b
mdeamf <mauriciodealmeidamf@gmail.com>
2021-05-20 14:57:52 -0300

    test: adding more tests to app.js

--- app.test.js
+++ app.test.js
@@ -1,5 +1,11 @@
 const app = require('./app');
-test('Somar 1 + 2', () => {
+
+test('Somar 1 + 2 = 3', () => {
   const result = app.sumNumbers(1, 2);
   expect(result).toEqual(3);
+});
+
+test('Somar 0 + 0 = 0', () => {
+  const result = app.sumNumbers(0, 0);
+  expect(result).toEqual(0);
 });
