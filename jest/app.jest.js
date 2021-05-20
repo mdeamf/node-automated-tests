c9849aab055f35f41b95b14d7d67739c995b91b5
mdeamf <mauriciodealmeidamf@gmail.com>
2021-05-20 15:48:00 -0300

    feat: adding callback async function

--- jest/app.jest.js
+++ jest/app.jest.js
@@ -4,6 +4,12 @@ const sumNumbers = (num1, num2) => {
 
 const concatStrings = (str1, str2) => {
   return str1 + ' ' + str2;
-}
+};
 
-module.exports = { sumNumbers, concatStrings };
+const runCallback = (cbk) => {
+  setTimeout(() => {
+    cbk('potato');
+  }, 2000);
+};
+
+module.exports = { sumNumbers, concatStrings, runCallback };
