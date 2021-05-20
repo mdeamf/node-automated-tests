6ae68131345813b3b3e9c889eed54d1cfb930129
mdeamf <mauriciodealmeidamf@gmail.com>
2021-05-20 15:20:50 -0300

    feat: adding a string concatenation function

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ const sumNumbers = (num1, num2) => {
   return num1 + num2;
 };
 
-module.exports = { sumNumbers };
+const concatStrings = (str1, str2) => {
+  return str1 + ' ' + str2;
+}
+
+module.exports = { sumNumbers, concatStrings };
